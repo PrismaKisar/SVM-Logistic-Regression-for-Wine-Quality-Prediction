fbf6b15d745556e8cd8a81fdf83a8b00d664f2a4
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2025-08-10 21:37:09 +0200

    feat: add SVM parameters

---
+++ src/models.py
@@ -0,0 +1,11 @@
+class SVM:
+    def __init__(self, n_iters=1000, lambda_param=0.01, random_seed=42):
+        self.n_iters = n_iters
+        self.lambda_param = lambda_param
+        self.random_seed = random_seed
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass

f731e80e45b6a92fc7b7491d75ed6ba8e8cfdb95
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2025-08-19 16:45:19 +0200

    fix: change variables name

--- src/util.py
+++ src/util.py
@@ -129,14 +129,14 @@ def calculate_metrics(predictions, y_true, metrics=['accuracy', 'precision', 're
 
 class StandardScaler:
     def __init__(self):
-        self.mean_ = None
-        self.scale_ = None
+        self._mean = None
+        self._scale = None
     
     def fit(self, X):
         X = X.values
         
-        self.mean_ = np.mean(X, axis=0)
-        self.scale_ = np.std(X, axis=0, ddof=1)
+        self._mean = np.mean(X, axis=0)
+        self._scale = np.std(X, axis=0, ddof=1)
         
         return self
     
@@ -145,7 +145,7 @@ class StandardScaler:
         columns = X.columns
         index = X.index
        
-        X_scaled = (X_values - self.mean_) / self.scale_
+        X_scaled = (X_values - self._mean) / self._scale
         
         if columns is not None:
             return pd.DataFrame(X_scaled, columns=columns, index=index)
